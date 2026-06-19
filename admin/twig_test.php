f1913421c19bb5d61975cceae4adeda2f5fff73a
galerror-admin <admin@galerror.top>
2026-06-19 08:11:31 +0200

    chore: 删除残留测试入口（任务1）

--- admin/twig_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'includes/config.php';
-require_once 'includes/user_auth.php';
-require_once 'includes/view.php';
-
-view('front/test.twig', [
-    'title' => 'Twig 接入成功',
-    'message' => 'Hello from Twig'
-]);
